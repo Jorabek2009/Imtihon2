6550e5410bf210bddeaabc7de7100b56f145f52c
jorabek <jorabekbozorov388@gmail.com>
2023-08-25 12:21:29 +0500

    imtihon

---
+++ jacasript/script.js
@@ -0,0 +1,80 @@
+// 1-masala
+// Object JavaScript dasturlash tili ichidagi ma'lumotlarni yig'ish va ishlab chiqish uchun ishlatiladigan asosiy obyektlardan biridir. Bu obyekt orqali barcha JavaScript obyektlarini yaratish, ishlatish va ulardan foydalanish mumkin.
+
+//2- masala
+// function result(n){
+//     let first = parseInt(n.toString()[0]); 
+//     let last = n % 10;
+//     let middle = parseInt(n.toString().slice(1, -1)); 
+//     let result = last.toString() + middle.toString() +     first.toString(); 
+//     console.log(result);
+// }
+// result(36985);
+
+
+
+// 3- masala
+// function swit(a){
+//     switch(a){
+//         case 1:
+//             console.log(`${a}-oy yanvar qish`);
+//         break;
+//         case 2:
+//             console.log(`${a}-oy fevral qish`);
+//         break;
+//         case 3:
+//             console.log(`${a}-oy mart bahor`);
+//         break;
+//         case 4:
+//             console.log(`${a}-oy aprel bahor`);
+//         break;
+//         case 5:
+//             console.log(`${a}-oy may bahor`);
+//         break;
+//         case 6:
+//             console.log(`${a}-oy iyun yoz`);
+//         break;
+//         case 7:
+//             console.log(`${a}-oy iyul yoz `);
+//         break;
+//         case 8:
+//             console.log(`${a}-oy avgust yoz`);
+//         break;
+//         case 9:
+//             console.log(`${a}-oy sentyabr kuz`);
+//         break;
+//         case 10:
+//             console.log(`${a}-oy oktabr kuz`);
+//         break;
+//         case 11:
+//             console.log(`${a}-oy noyabr kuz`);
+//         break;
+//         case 12:
+//             console.log(`${a}- dekabr qish`);
+//         break;
+//     }
+// }
+// swit(3);
+
+
+
+
+// 4- masala
+
+// function add(n){
+//     let onlik = parseInt((n / 10) % 10);
+//     console.log(onlik); 
+// }
+// add(12345);
+
+
+//5-masala
+// Function JavaScript dasturlash tili yordamida yaratilgan bir obyekt hisoblanadi .Function yaratilishi:
+
+// 1. Function nomi yoziladi.
+// 2. () ichida funksiyaga kerak bo'lgan argumentlar yoziladi.
+// 3. {} ichida funksiyani bajaradigan kodlar yoziladi.
+// 4. Agar funksiyada qiymat qaytarish kerak bo'lsa, return operatori yoziladi.
+
+// Functionlar o'zgaruvchilarni qabul qilishi mumkin, uni o'z ichiga olishi mumkin va uni boshqa funksiyalarda ishlatishi mumkin. Functionlarni  qayta ishlatish mumkin va kodni aniqroq qiladi.
+
